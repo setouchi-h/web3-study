2b5d157405a7606d7193564332fb940b11bf1204
setouchi-h <setouchi.kazuki@gmail.com>
2023-04-05 08:51:44 +0900

    create create2

---
+++ create2/contracts/Factory.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "./TestContract.sol";
+
+contract Factory {
+    function deploy(
+        address _owner,
+        uint _foo,
+        bytes32 _salt
+    ) public payable returns (address) {
+        // This syntax is a newer way to invoke create2 without assembly, you just need to pass salt
+        // https://docs.soliditylang.org/en/latest/control-structures.html#salted-contract-creations-create2
+        return address(new TestContract{salt: _salt}(_owner, _foo));
+    }
+}
