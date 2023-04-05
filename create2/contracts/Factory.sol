036b5ec7b31d69c13fdd141108259d7d0b3f4dbf
setouchi-h <setouchi.kazuki@gmail.com>
2023-04-05 18:38:32 +0900

    update factory

--- create2/contracts/Factory.sol
+++ create2/contracts/Factory.sol
@@ -8,9 +8,9 @@ contract Factory {
         address _owner,
         uint _foo,
         bytes32 _salt
-    ) public payable returns (address) {
+    ) public payable returns (TestContract) {
         // This syntax is a newer way to invoke create2 without assembly, you just need to pass salt
         // https://docs.soliditylang.org/en/latest/control-structures.html#salted-contract-creations-create2
-        return address(new TestContract{salt: _salt}(_owner, _foo));
+        return new TestContract{salt: _salt}(_owner, _foo);
     }
 }
